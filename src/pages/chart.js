b8f42ad9f87fb9c58e09586f79e85e3a21a84275
Pradeep <114543921+pradeep-08@users.noreply.github.com>
2024-07-12 18:10:38 +0530

    Update chart.js

--- src/pages/chart.js
+++ src/pages/chart.js
@@ -3,27 +3,31 @@ import "../pages/chart.css";
 import Bar1 from "./bar1";
 import Bar2 from "./bar2";
 
-
-
-
-
-
-
-
 function Chart() {
-
-  return( 
+  return (
     <>
       <div className="master-container">
         <div className="main-div-chart-layout">
           <div className="main-chart-container">
-           <Bar1/>   
+            <Bar1 />
           </div>
           <div className="main-chart-container-2">
-          <div className="bar-container">
-           <Bar2/>
-          </div>
-           
+            <div className="bar-container">
+              <Bar2 />
+              <div className="semi-bar-details">
+                <div>
+                  <div className="b1">
+                    <div className="box-1"></div>Completed
+                  </div>
+                  <div className="b2">
+                    <div className="box-1"></div>In-process
+                  </div>
+                  <div className="b3">
+                    <div className="box-1"></div>Not-yet
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
