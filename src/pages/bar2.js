50b018078522586df50f472e37ea508ee928ae6c
Pradeep <114543921+pradeep-08@users.noreply.github.com>
2024-07-12 18:08:40 +0530

    Update bar2.js

--- src/pages/bar2.js
+++ src/pages/bar2.js
@@ -1,40 +1,42 @@
 import React from "react";
 import ReactApexChart from "react-apexcharts";
+import "../pages/chart.css";
 
 class ApexChart extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      series: [78, 55, 17],
+      name: "Location-1",
+      series: [78, 85, 59],
       options: {
         chart: {
-          height: 500, // Increase the height of the chart
-          type: 'radialBar',
-          offsetY: -10, // Adjusts vertical position to center
+          height: 400, // Increase the height of the chart
+          type: "radialBar",
+          offsetY: -15, // Adjusts vertical position to center
         },
         plotOptions: {
           radialBar: {
             startAngle: -90, // Starting angle to make it semi-circle
-            endAngle: 90,   // Ending angle to make it semi-circle
+            endAngle: 90, // Ending angle to make it semi-circle
             hollow: {
-              margin: 0,
-              size: "40%", // Size of the inner circle
+              margin: 90,
+              size: "60%", // Size of the inner circle
             },
             dataLabels: {
               name: {
-                show: false,
+                show: true,
               },
               value: {
                 show: true,
                 offsetY: 10,
-                fontSize: '22px',
-              }
-            }
-          }
+                fontSize: "22px",
+              },
+            },
+          },
         },
-        colors: [  "#EF6416", "#804B2D","#1B56ED"], // Customize bar colors here
-        labels: ['Location', 'location2', 'Location3'],
+        colors: ["#EF6416", "#804B2D", "#1B56ED"], // Customize bar colors here
+        labels: ["Location", "location-2", "Location-3"],
       },
     };
   }
@@ -43,7 +45,14 @@ class ApexChart extends React.Component {
     return (
       <div>
         <div id="chart">
-          <ReactApexChart options={this.state.options} series={this.state.series} type="radialBar" height={350} />
+          <div className="overview">Summary</div>
+          <ReactApexChart
+            options={this.state.options}
+            series={this.state.series}
+            type="radialBar"
+            height={400}
+            width={450}
+          />
         </div>
         <div id="html-dist"></div>
       </div>
