3f47fe6b9434c45c40aba2871874ebc859d90979
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2024-07-14 23:33:02 +0530

    update in ticket page

--- src/pages/service/tickets.js
+++ src/pages/service/tickets.js
@@ -4,7 +4,6 @@ import "./tickets.css";
 function tickets() {
   return (
     <div>
-      
       <div className="second-nav-bar">
         <div className="dashboard-child">
           <span className="dashboard-name-1">Tickets</span>
@@ -25,9 +24,255 @@ function tickets() {
           </div>
         </div>
       </div>
+      <div className="master-main-page-continer">
+        <div className="main-page-container">
+          <div className="main-page-1">
+            <div className="child-main-page">
+              <div className="color-tittle">
+                <div className="color-box-1"></div>
+                <span className="color-name">Not occupied</span>
+              </div>
+              <div className="color-tittle">
+                <div className="color-box-2"></div>
+                <span className="color-name">Completed</span>
+              </div>
+              <div className="color-tittle">
+                <div className="color-box-3"></div>
+                <span className="color-name">Occupied</span>
+              </div>
+              <div className="color-tittle">
+                <div className="color-box-4"></div>
+                <span className="color-name">In-progress</span>
+              </div>
+            </div>
+          </div>
+          <div className="main-page-2">
+            <div className="progressing-data-bar-main-container">
+              <div className="bar-title-name-div">
+                <span>1st Floor</span>
+              </div>
+              <div className="master-bar-container">
+                <div className="main-bar-div">
+                  <div className="data-bar-parent">
+                    <div className="data-bar-1">D1</div>
+                    <div className="data-bar-2">D2</div>
+                    <div className="data-bar-3">D3</div>
+                    <div className="data-bar-4">D4</div>
+                    <div className="data-bar-1">D5</div>
+                    <div className="data-bar-3">D6</div>
+                    <div className="data-bar-4">D7</div>
+                    <div className="data-bar-2">D8</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="progressing-data-bar-main-container">
+              <div className="bar-title-name-div">
+                <span>2nd Floor</span>
+              </div>
+              <div className="master-bar-container">
+                <div className="main-bar-div">
+                  <div className="data-bar-parent">
+                    <div className="data-bar-1">D1</div>
+                    <div className="data-bar-1">D2</div>
+                    <div className="data-bar-4">D3</div>
+                    <div className="data-bar-4">D4</div>
+                    <div className="data-bar-3">D5</div>
+                    <div className="data-bar-4">D6</div>
+                    <div className="data-bar-4">D7</div>
+                    <div className="data-bar-4">D8</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="progressing-data-bar-main-container">
+              <div className="bar-title-name-div">
+                <span>3rd Floor</span>
+              </div>
+              <div className="master-bar-container">
+                <div className="main-bar-div">
+                  <div className="data-bar-parent">
+                    <div className="data-bar-1">D1</div>
+                    <div className="data-bar-2">D2</div>
+                    <div className="data-bar-1">D3</div>
+                    <div className="data-bar-1">D4</div>
+                    <div className="data-bar-1">D5</div>
+                    <div className="data-bar-3">D6</div>
+                    <div className="data-bar-4">D7</div>
+                    <div className="data-bar-1">D8</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="block-name">Block 1</div>
+        </div>
+        <div className="main-page-container">
+          <div className="main-page-1">
+            <div className="child-main-page">
+              <div className="color-tittle">
+                <div className="color-box-1"></div>
+                <span className="color-name">Not occupied</span>
+              </div>
+              <div className="color-tittle">
+                <div className="color-box-2"></div>
+                <span className="color-name">Completed</span>
+              </div>
+              <div className="color-tittle">
+                <div className="color-box-3"></div>
+                <span className="color-name">Occupied</span>
+              </div>
+              <div className="color-tittle">
+                <div className="color-box-4"></div>
+                <span className="color-name">In-progress</span>
+              </div>
+            </div>
+          </div>
+          <div className="main-page-2">
+            <div className="progressing-data-bar-main-container">
+              <div className="bar-title-name-div">
+                <span>1st Floor</span>
+              </div>
+              <div className="master-bar-container">
+                <div className="main-bar-div">
+                  <div className="data-bar-parent">
+                    <div className="data-bar-1">D1</div>
+                    <div className="data-bar-2">D2</div>
+                    <div className="data-bar-3">D3</div>
+                    <div className="data-bar-3">D4</div>
+                    <div className="data-bar-3">D5</div>
+                    <div className="data-bar-3">D6</div>
+                    <div className="data-bar-3">D7</div>
+                    <div className="data-bar-2">D8</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="progressing-data-bar-main-container">
+              <div className="bar-title-name-div">
+                <span>2nd Floor</span>
+              </div>
+              <div className="master-bar-container">
+                <div className="main-bar-div">
+                  <div className="data-bar-parent">
+                    <div className="data-bar-1">D1</div>
+                    <div className="data-bar-2">D2</div>
+                    <div className="data-bar-2">D3</div>
+                    <div className="data-bar-4">D4</div>
+                    <div className="data-bar-1">D5</div>
+                    <div className="data-bar-1">D6</div>
+                    <div className="data-bar-4">D7</div>
+                    <div className="data-bar-2">D8</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="progressing-data-bar-main-container">
+              <div className="bar-title-name-div">
+                <span>3rd Floor</span>
+              </div>
+              <div className="master-bar-container">
+                <div className="main-bar-div">
+                  <div className="data-bar-parent">
+                    <div className="data-bar-1">D1</div>
+                    <div className="data-bar-2">D2</div>
+                    <div className="data-bar-3">D3</div>
+                    <div className="data-bar-1">D4</div>
+                    <div className="data-bar-1">D5</div>
+                    <div className="data-bar-3">D6</div>
+                    <div className="data-bar-4">D7</div>
+                    <div className="data-bar-2">D8</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="block-name">Block 2</div>
+        </div>
+        <div className="main-page-container">
+          <div className="main-page-1">
+            <div className="child-main-page">
+              <div className="color-tittle">
+                <div className="color-box-1"></div>
+                <span className="color-name">Not occupied</span>
+              </div>
+              <div className="color-tittle">
+                <div className="color-box-2"></div>
+                <span className="color-name">Completed</span>
+              </div>
+              <div className="color-tittle">
+                <div className="color-box-3"></div>
+                <span className="color-name">Occupied</span>
+              </div>
+              <div className="color-tittle">
+                <div className="color-box-4"></div>
+                <span className="color-name">In-progress</span>
+              </div>
+            </div>
+          </div>
+          <div className="main-page-2">
+            <div className="progressing-data-bar-main-container">
+              <div className="bar-title-name-div">
+                <span>1st Floor</span>
+              </div>
+              <div className="master-bar-container">
+                <div className="main-bar-div">
+                  <div className="data-bar-parent">
+                    <div className="data-bar-4">D1</div>
+                    <div className="data-bar-2">D2</div>
+                    <div className="data-bar-3">D3</div>
+                    <div className="data-bar-4">D4</div>
+                    <div className="data-bar-1">D5</div>
+                    <div className="data-bar-3">D6</div>
+                    <div className="data-bar-1">D7</div>
+                    <div className="data-bar-3">D8</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="progressing-data-bar-main-container">
+              <div className="bar-title-name-div">
+                <span>2nd Floor</span>
+              </div>
+              <div className="master-bar-container">
+                <div className="main-bar-div">
+                  <div className="data-bar-parent">
+                    <div className="data-bar-1">D1</div>
+                    <div className="data-bar-1">D2</div>
+                    <div className="data-bar-3">D3</div>
+                    <div className="data-bar-4">D4</div>
+                    <div className="data-bar-1">D5</div>
+                    <div className="data-bar-3">D6</div>
+                    <div className="data-bar-4">D7</div>
+                    <div className="data-bar-1">D8</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="progressing-data-bar-main-container">
+              <div className="bar-title-name-div">
+                <span>3rd Floor</span>
+              </div>
+              <div className="master-bar-container">
+                <div className="main-bar-div">
+                  <div className="data-bar-parent">
+                    <div className="data-bar-1">D1</div>
+                    <div className="data-bar-2">D2</div>
+                    <div className="data-bar-3">D3</div>
+                    <div className="data-bar-4">D4</div>
+                    <div className="data-bar-1">D5</div>
+                    <div className="data-bar-3">D6</div>
+                    <div className="data-bar-4">D7</div>
+                    <div className="data-bar-2">D8</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="block-name">Block 3</div>
+        </div>
 
-
-     
+      </div>
     </div>
   );
 }
